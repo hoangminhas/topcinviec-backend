d59bb106f1ae36de7b00182da9fd0bd1c5f3322e
trieu.long <long.trieu.hr@gmail.com>
2022-04-12 16:43:26 +0700

    create AuthService

---
+++ app/Services/AuthService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\AuthRepository;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthService extends BaseService
+{
+    public $authRepository;
+
+    public function __construct(AuthRepository $authRepository)
+    {
+        $this->authRepository = $authRepository;
+    }
+
+    public function login($request)
+    {
+        $data = $request->only('email','password');
+        if(Auth::attempt($data)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+    }
+
+  
+}
