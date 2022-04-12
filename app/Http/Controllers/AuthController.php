f4a76d02ce5ef8ccaf424e3252d783584836ada2
trieu.long <long.trieu.hr@gmail.com>
2022-04-12 16:32:52 +0700

    create BaseService

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+
 class AuthController extends Controller
 {
-   
+    public function login (Request $request)
+    {
+        $user = $request->only('name','email');
+        if ( Auth ::attempt($user))
+        {
+            return response()->json(
+                [
+                    'status'=>201,
+                    'msg'=> 'Dang nhap thanh cong'
+                ]
+            );
+        }
+        else
+        {
+            return response()->json(
+                [
+                    'status'=>false,
+                    'msg'=>'Sai ten tai khoan hoac mat khau',
+                ]
+            );
+        }
+    }
 }
