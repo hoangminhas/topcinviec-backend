cbdc241488d5e3cfa004e36e4784f58dc738e569
Thuy <thuyle29111995@gmail.com>
2022-04-14 11:41:03 +0700

    overwrite

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('login',[AuthController::class,'showFormLogin'])->name('showFormLogin');
+Route::post('login', [AuthController::class,'login'])->name('login');
+
+Route::post('/register', function () {
+    return view('registration');
 });
